1344dd9ab0a7e356ef4d29920234d8078d0be3c5
Alessandro-Ampala-JCMaxwell-4BI <alessandro.ampala@jcmaxwell.it>
2017-05-19 23:49:04 +0200

    Aggiunto monitor (esercizio ultimato)
    -Aggiunta classe Monitor per la gestione di risorse condivise tra i vari threads
    -Ora le risorse condivise tra i vari threads vengono gestite tramite un oggetto della classe Monitor che usa un metodo synchronized per accedere ad esse

--- MultiThread.java
+++ MultiThread.java
@@ -8,22 +8,25 @@
  * 6. posso passare parametri al THREADs tramite il costruttore della classe Runnable
  */
 
+package multi;
+
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
 public class MultiThread {
+
 
     // "main" e' il THREAD principale da cui vengono creati e avviati tutti gli altri THREADs
     // i vari THREADs poi evolvono indipendentemente dal "main" che puo' eventualmente terminare prima degli altri
     public static void main(String[] args) {
         System.out.println("Main Thread iniziata...");
         long start = System.currentTimeMillis();
-        Monitor monitor = new Monitor();
+        Monitor m = new Monitor(); //monitor per controllare l'accesso alle risorse condivise
         
         // Creazione threads
-        Thread tic = new Thread (new TicTacToe("TIC", monitor));
-        Thread tac = new Thread(new TicTacToe("TAC", monitor));
-        Thread toe = new Thread(new TicTacToe("TOE", monitor));
+        Thread tic = new Thread (new TicTacToe("TIC", m));
+        Thread tac = new Thread(new TicTacToe("TAC", m));
+        Thread toe = new Thread(new TicTacToe("TOE", m));
         
         //Avvio threads
         toe.start();
@@ -32,13 +35,15 @@ public class MultiThread {
         
         // Attendo che l'esecuzione di ogni thread finisca per poter andare avanti con il codice
         try{
+            tic.join();
+            tac.join();
             toe.join();
         }catch (InterruptedException e) {}
         
         
         long end = System.currentTimeMillis();
         System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms");
-        System.out.println("Punteggio: " + TicTacToe.punteggio); // Stampa del punteggio
+        System.out.println("Punteggio: " + m.getPunteggio()); // Stampa del punteggio
     }
 }
 
@@ -50,47 +55,11 @@ class TicTacToe implements Runnable {
     
      // per le variabili static c'è una variabile in comune a tutti i threads, mentre per le altre ce n'è una copia per ogni thread
     private String t;
-    private String msg;
-    public static int punteggio = 0;
-    Monitor m;
+    public String msg;
+    private Monitor m;
 
     // Costruttore, possiamo usare il costruttore per passare dei parametri al THREAD
     public TicTacToe (String s, Monitor m) {
-    	this.m = m;
+        this.m = m;
         this.t = s;
     }
-    
-    
-    @Override // Annotazione per il compilatore
-    // se facessimo un overloading invece di un override il copilatore ci segnalerebbe l'errore
-    // per approfondimenti http://lancill.blogspot.it/2012/11/annotations-override.html
-    public void run() {
-        for (int i = 10; i > 0; i--) {
-        	msg = "<" + t + "> " + t + ": " + i;
-            m.Scrivi(t, msg);
-        }
-    }
-}
-
-class Monitor{
-	int punteggio;
-    private Random rand = new Random(); //oggetto Random per generazione di numeri random
-    private static String precedente = "";
-    
-    
-    
-	public synchronized void Scrivi(String t, String msg) //serve ad evitare i conflitti
-    {
-    	try {
-            int pickedNumber = rand.nextInt(300) + 100; 
-            TimeUnit.MILLISECONDS.sleep(pickedNumber);
-        } catch (InterruptedException e) {
-            System.out.println("THREAD " + t + " e' stata interrotta! bye bye...");
-            return; //me ne vado = termino il THREAD
-        }
-        if(t.equals("TOE") && precedente.equals("TAC"))
-            punteggio ++;
-        precedente = t;
-        System.out.println(msg);
-    }
-}
